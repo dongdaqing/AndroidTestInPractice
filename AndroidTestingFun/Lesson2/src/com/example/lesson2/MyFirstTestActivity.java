6ec88399922586cf7bafa3d7626685642072202b
dongdaqing <ddaqing@gmail.com>
2014-08-30 15:46:49 +0800

    Android Testing Fun

---
+++ AndroidTestingFun/Lesson2/src/com/example/lesson2/MyFirstTestActivity.java
@@ -0,0 +1,15 @@
+package com.example.lesson2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MyFirstTestActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_my_first_test);
+	}
+}
---
+++ AndroidTestingFun/README.md
@@ -0,0 +1,6 @@
+AndroidTestingFun
+=====================
+
+## Overview
+
+Android Unit Test Demo Study. Reference:http://developer.android.com/training/testing.html
